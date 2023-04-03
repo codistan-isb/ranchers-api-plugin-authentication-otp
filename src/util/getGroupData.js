c17e356fd72f31ce94a462b86c780440f296de3d
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-04-03 13:40:35 +0500

    New Group ID Added on acount Creation

--- src/util/getGroupData.js
+++ src/util/getGroupData.js
@@ -2,8 +2,13 @@ export async function getGroupData(GroupName, Groups) {
 
     console.log("Group Name : ", GroupName)
     console.log(Groups)
-    if (GroupName === 'dispatcher' || GroupName === 'admin') {
+    if (GroupName === 'dispatcher') {
         const GroupDataResp = await Groups.findOne({ name: GroupName });
+        console.log("Group Data Resp: ", GroupDataResp._id)
+        return GroupDataResp._id;
+    }
+    if (GroupName === 'admin') {
+        const GroupDataResp = await Groups.findOne({ name: "branch admin" });
         console.log("Group Data Resp: ", GroupDataResp._id)
         return GroupDataResp._id;
     }
