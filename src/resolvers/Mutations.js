3d60a85a13667fcb5f0a0b10d352f0cd295c55f3
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-05-17 21:25:18 +0500

    createUser API Updated

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -100,11 +100,11 @@ export default {
   async createUser(_, { user }, ctx) {
     // console.log(user);
     const { injector, infos, collections } = ctx;
-    const { Accounts, Groups } = collections;
+    const { Accounts, Groups, BranchData } = collections;
     const accountsServer = injector.get(server_1.AccountsServer);
     const accountsPassword = injector.get(password_1.AccountsPassword);
     let userId;
-
+    let AllBranchIDs;
     if (!user.UserRole) {
       try {
         userId = await accountsPassword.createUser(user);
@@ -177,12 +177,94 @@ export default {
       await generateOtp(user.phone);
       return {
         userId,
-        // loginResult,
+        loginResult,
+      };
+    } else if (user.UserRole === 'admin') {
+      if (!ctx.authToken) {
+        throw new ReactionError("access-denied", "Please Login First");
+      }
+      if (ctx.user === undefined || ctx.user === null) {
+        throw new ReactionError("access-denied", "Please Login First");
+      }
+      const GroupNameResp = await getGroupData(user.UserRole, Groups);
+      // console.log("Group Name Resp in return :-", GroupNameResp);
+      const branchData = await BranchData.find({}).toArray();
+      // console.log("branch Data ", branchData);
+      if (branchData) {
+        AllBranchIDs = branchData.map(data => data._id);
+        // console.log("AllBranchIDs ", AllBranchIDs);
+        // userId = await accountsPassword.createUser(user);
+        user.branches = AllBranchIDs;
+        console.log("branches ", user);
+      }
+      else {
+        user.branches = [];
+      }
+
+      userId = await accountsPassword.createUser(user);
+      if (!accountsServer.options.enableAutologin) {
+        return {
+          userId: accountsServer.options.ambiguousErrorMessages
+            ? null
+            : userId,
+        };
+      }
+      if (userId) {
+        const now = new Date();
+        const account = {
+          "_id": userId,
+          "acceptsMarketing": false,
+          "emails": [
+            {
+              "address": user.email,
+              "verified": false,
+              "provides": "default"
+            }
+          ],
+          "groups": [GroupNameResp],
+          "name": null,
+          "profile": {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            dob: user.dob,
+            phone: user.phone,
+          },
+          "shopId": null,
+          "state": "new",
+          "userId": userId,
+          "UserRole": user.UserRole,
+          "currentStatus": "online",
+          "createdAt": now,
+          "branches": user.branches
+        }
+        // const accountAdded = await Accounts.insertOne({
+        //   _id: userId,
+        //   firstName: user.firstName,
+        //   lastName: user.lastName,
+        //   name: user.firstName + " " + user.lastName,
+        //   phone: user.phone,
+        //   UserRole: user.UserRole
+        // });
+        const accountAdded = await Accounts.insertOne(account);
+        console.log("account Added:- ", accountAdded)
+      }
+      // When initializing AccountsServer we check that enableAutologin and ambiguousErrorMessages options
+      // are not enabled at the same time
+      const createdUser = await accountsServer.findUserById(userId);
+      // If we are here - user must be created successfully
+      // Explicitly saying this to Typescript compiler
+      const loginResult = await accountsServer.loginWithUser(
+        createdUser,
+        infos
+      );
+      await generateOtp(user.phone);
+      return {
+        userId,
+        loginResult,
       };
     } else {
       if (!ctx.authToken) {
         throw new ReactionError("access-denied", "Please Login First");
-
       }
       if (ctx.user === undefined || ctx.user === null) {
         throw new ReactionError("access-denied", "Please Login First");
@@ -197,7 +279,7 @@ export default {
       const UserPermission = canCreateUser(ctx.user.UserRole, user.UserRole);
       // console.log(UserPermission);
       const GroupNameResp = await getGroupData(user.UserRole, Groups)
-      // console.log("Group Name Resp in return :-", GroupNameResp)
+      console.log("Group Name Resp in return :-", GroupNameResp)
       if (UserPermission) {
         // Allow user creation
         try {
@@ -274,7 +356,7 @@ export default {
         await generateOtp(user.phone);
         return {
           userId,
-          // loginResult,
+          loginResult,
         };
       } else {
         // Deny user creation
